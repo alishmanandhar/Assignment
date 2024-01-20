132695efd2282c093ef6c5ad4f83bb3b963a27cb
alishmanandhar <alish7manandhar@gmail.com>
2024-01-20 12:23:17 +0545

    finished pulling users

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,9 +1,27 @@
-import { Modal } from "@repo/assignment-ui/modal";
+import { Button } from "@repo/ui/button";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
 
 export default function Page(): JSX.Element {
   return (
     <main>
       <div className="bg-green-500">Web Page</div> 
+      <Card className="w-80 bg-green-100" >
+        <CardHeader>
+          <CardTitle>24 Birthdays Today</CardTitle>
+        </CardHeader>
+        <CardContent>
+          asdf
+        </CardContent>
+        <CardFooter>
+          <p>Card Footer</p>
+        </CardFooter>
+      </Card>
     </main>
   );
 }
--- packages/assignment-ui/src/modal.tsx
+++ packages/assignment-ui/src/modal.tsx
@@ -1,15 +1,32 @@
 "use client";
 
 import { ReactNode } from "react";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card"
+import { Button } from "./button";
 
 interface ModalProps {
   children: ReactNode;
+  className?: string;
 }
 
-export const Modal = ({ children }: ModalProps) => {
+export const Modal = ({ children,className }: ModalProps) => {
   return (
-    <>
+    <Card className={"w-96 shadow-2xl"} >
+      <CardHeader>
+        <CardTitle className="text-center">24 Birthdays Today</CardTitle>
+      </CardHeader>
+      <CardContent>
         {children}
-    </>
+      </CardContent>
+      <CardFooter>
+        <Button>View All</Button>
+      </CardFooter>
+    </Card>
   );
 };
